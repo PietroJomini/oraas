8e172e787d381d04e57dd5b08ac665ef0a696d16
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-10-13 16:18:40 +0200

    url params

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 import { render } from "./3d.js";
 
+const params = new URLSearchParams(window.location.search);
+
+const OBJPath = params.get("obj");
+const MTLPath = params.get("mtl");
+const background = params.get("background");
+
 render({
   DOMElement: document.querySelector("#mod"),
-  OBJPath: "../model/PRAILLON_06082020_simplified_3d_mesh.obj",
-  MTLPath: "../model/PRAILLON_06082020_simplified_3d_mesh.mtl",
-  background: 0x121212,
-  lightIntensity: 0.9,
+  OBJPath,
+  MTLPath,
+  background,
 });
