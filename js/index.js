aa4c5bbc110a1acf95b53427f1284590f4033e02
Pietro Jomini <pietro.jomini@protonmail.ch>
2020-10-13 17:43:37 +0200

    fetching to fix corse errors (!\?)

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
-import { render } from "./3d.js";
+import { render } from './3d.js';
 
 const params = new URLSearchParams(window.location.search);
 
-const OBJPath = params.get("obj");
-const MTLPath = params.get("mtl");
-const background = `#${params.get("background")}`;
-const fov = params.get("fov");
-const lightSkyColor = params.get("lightSkyColor");
-const lightGroudnColor = params.get("lightGroundColor");
-const fitRatio = params.get("fitRatio");
+const OBJPath = params.get('obj');
+const MTLPath = params.get('mtl');
+const background = `#${params.get('background')}`;
+const fov = params.get('fov');
+const lightSkyColor = params.get('lightSkyColor');
+const lightGroudnColor = params.get('lightGroundColor');
+const fitRatio = params.get('fitRatio');
+
+fetch(MTLPath);
+fetch(OBJPath);
 
 render({
-  DOMElement: document.querySelector("#mod"),
+  DOMElement: document.querySelector('#mod'),
   OBJPath,
   MTLPath,
   background,
